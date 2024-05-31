7fab10d372903bfbcf033f21f546245cf940e173
mrAntGit <xollusinchi@gmail.com>
2024-05-30 23:20:08 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,12 +30,12 @@ function setup() {
 function draw() {
   background(0); // Set background to black
   
-  // Draw the spinning ornament
+  
   let ornamentSize = 30;
   let ornamentAngle = millis() * 0.0001; // Control spinning speed
   drawOrnament(0, 0, ornamentSize, ornamentAngle);
 
-  // Draw lines feeding into the ornament
+  
   for (let i = 0; i < numLines; i++) {
     let angle = i * angleIncrement + angleOffset;
     let x1 = radius * cos(angle);
@@ -43,11 +43,11 @@ function draw() {
     let x2 = ornamentSize / 2 * cos(ornamentAngle);
     let y2 = ornamentSize / 2 * sin(ornamentAngle);
     
-    stroke(255); // Set line color to white
+    stroke(255);
     line(x1, y1, x2, y2);
   }
   
-  angleOffset += 0.01; // Rotate the lines
+  angleOffset += 0.01; 
   
   spaceman.update();
   spaceman.draw();
@@ -62,7 +62,6 @@ function drawOrnament(x, y, size, angle) {
   push();
   translate(x, y);
   rotate(angle);
-  // Draw your ornament shape here
   fill(0);
   ellipse(0, 0, size, size);
   pop();
